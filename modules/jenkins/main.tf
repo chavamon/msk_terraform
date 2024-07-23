8ef6084e33dd1b9ba5cfbbc2b36d235cbd02cd73
Salvador Valeriano lucas <salvador.valeriano@gmail.com>
2024-07-22 22:04:36 -0600

    configuration of jenkins

--- modules/jenkins/main.tf
+++ modules/jenkins/main.tf
@@ -37,21 +37,36 @@ resource "aws_instance" "jenkins_instance" {
   tags = {
     Name = "JenkinsInstance"
   }
-
   provisioner "remote-exec" {
     inline = [
       "sudo yum update -y",
       "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
       "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
       "sudo yum upgrade -y",
+      "sudo yum install -y git", # Git installation command added here
       "sudo wget https://corretto.aws/downloads/latest/amazon-corretto-21-x64-linux-jdk.rpm",
       "sudo rpm -i amazon-corretto-21-x64-linux-jdk.rpm",
       "sudo yum install -y jenkins",
       "sudo yum install -y fontconfig",
+      # Install Maven
+      "sudo wget https://dlcdn.apache.org/maven/maven-3/3.9.8/binaries/apache-maven-3.9.8-bin.tar.gz -P /tmp",
+      "sudo tar xf /tmp/apache-maven-*.tar.gz -C /opt",
+      "sudo ln -s /opt/apache-maven-3.9.8 /opt/maven",
+      "echo 'export M2_HOME=/opt/maven' | sudo tee -a /etc/profile.d/maven.sh",
+      "echo 'export PATH=$${M2_HOME}/bin:$${PATH}' | sudo tee -a /etc/profile.d/maven.sh",
+      "sudo chmod +x /etc/profile.d/maven.sh",
+      "source /etc/profile.d/maven.sh",
+      # Set JAVA_HOME for Jenkins
+      "echo 'JAVA_HOME=/usr/lib/jvm/java-21-amazon-corretto' | sudo tee -a /etc/environment",
       "echo 'JENKINS_JAVA_OPTIONS=\"-Djava.awt.headless=true\"' | sudo tee -a /etc/sysconfig/jenkins",
       "sudo systemctl daemon-reload",
-      "sudo systemctl start jenkins",
-      "sudo systemctl enable jenkins"
+      "sudo systemctl restart jenkins",
+      "sudo systemctl enable jenkins",
+
+      # Install Docker
+      "sudo yum install -y docker",
+      "sudo systemctl start docker",
+      "sudo systemctl enable docker"
     ]
 
     connection {
@@ -61,6 +76,7 @@ resource "aws_instance" "jenkins_instance" {
       host        = self.public_ip
     }
   }
+
 }
 
 output "jenkins_instance_public_ip" {
